fecde8a132b087e10a3a38aafffd1a612a216a28
Jorge Martin <jorgemctin@gmail.com>
2023-09-15 11:14:51 +0200

    Refactor: contact page with dark hole effect

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -7,14 +7,24 @@ export const Contact = () => {
         <div className="containerContact">
 
             <div className="contactData">
-                <h2>Contact</h2>
+            
+            
+                <div className="dataData"><strong>
+                {/* <h3 style={{
+                    transform:'none',
+
+                }}>Contact</h3> */}
                 <p>Jorge Luis Martin Lorenzo</p>
                 <p>Full Stack Developer</p>
                 <p>Valencia, Spain</p>
                 <p>jorgemctin@gmail.com</p>
                 {/* <p>Teléfono: +123 456 7890</p> */}
-                <div className="buttonsContact">
-                <PortfolioButton
+                </strong>
+     
+            </div></div>
+            <div className="buttonsContact">
+                
+            <PortfolioButton
                     href={"https://www.linkedin.com/in/jorge-luis-martin-lorenzo/"}
                     text={"LinkedIn"}
                     imageSrc={""}
@@ -23,8 +33,13 @@ export const Contact = () => {
                     href={"https://github.com/jorgemctin"}
                     text={"GitHub"}
                     imageSrc={""}
-                /></div>
-            </div>
+                />
+                {/* <PortfolioButton
+                    href={"src/assets/Jorge"}
+                    text={"CV"}
+                    imageSrc={""}
+                /> */}
+                </div>
         </div>
     );
 };
