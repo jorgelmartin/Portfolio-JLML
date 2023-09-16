f9fb2bc021f805efc7553fd6a5530b3a20954de2
Jorge Martin <jorgemctin@gmail.com>
2023-09-16 20:26:04 +0200

    Refactor: PortfolioButton

--- src/components/PortfolioButton/PortfolioButton.jsx
+++ src/components/PortfolioButton/PortfolioButton.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import './PortfolioButton.css';
 
+//PORTFOLIO BUTTON
 export const PortfolioButton = ({ onClick, imageSrc, text, href }) => {
-
     return (
-        <>
-<div className='borderAnother'>
+        <div className='borderAnother'>
             <a href={href} target="_blank" rel="noopener noreferrer">
                 <button className="ButtonPortfolio" onClick={onClick}>
                     {text}
-                    {imageSrc && <img src={imageSrc} alt="" className="ButtonImage" style={{ maxWidth: '1.5em', maxHeight: '1.5em' }}/>}
+                    {imageSrc && <img src={imageSrc} alt="" className="ButtonImage" style={{ maxWidth: '1.5em', maxHeight: '1.5em' }} />}
                 </button>
-            </a></div>
-        </>
+            </a>
+        </div>
     )
 }
