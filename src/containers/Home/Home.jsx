2efdf3c7c34e70be1eb42df6d458f25502ff91b2
Jorge Martin <jorgemctin@gmail.com>
2023-09-16 20:28:05 +0200

    Feat: Home page wiith my name on it

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './Home.css';
+import { Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+export const Home = () => {
+    const navigate = useNavigate(); // Obtiene la función de navegación
+
+    const handleHomeDesignClick = () => {
+        // Redirige al usuario a la página Projects al hacer clic en homeDesign
+        navigate("/projects");
+    };
+  
+
+    return (
+        <>
+            <div className="containerHome" onClick={handleHomeDesignClick}>
+            <h1 className="nameStyle">PORTFOLIO</h1>
+            <h1 className="nameStyle">JORGE LUIS MARTIN LORENZO</h1>
+                <div className="homeDesign">
+                    
+                </div>
+            </div>
+        </>
+    )
+}
